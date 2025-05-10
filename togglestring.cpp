2d09b2df270b2c062112ea7a4aae2680509a9ccc
dhananjay ingole <145216339+dhananjayingole@users.noreply.github.com>
2025-05-10 22:24:15 +0530

    Add files via upload

---
+++ togglestring.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+string str;
+getline(cin,str);
+for(int i=0;str[i]!='\0';i++){
+if(islower(str[i])){
+    str[i]=toupper(str[i]);
+}
+else if (isupper(str[i])){
+    str[i]=tolower(str[i]);
+}
+}
+cout<<"Toggled string :"<<str<<endl;
+
+   return 0;
+}
