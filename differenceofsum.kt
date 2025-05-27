eadd1e3220258058ad68d30b787431796c5d25bf
Dhananjay Ingole <145216339+dhananjayingole@users.noreply.github.com>
2025-05-27 21:56:46 +0530

    Create differenceofsum.kt

---
+++ differenceofsum.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun differenceOfSum(n: Int, m: Int): Int {
+        var num1 = 0
+        var num2 = 0
+
+        for (i in 1..n) {
+            if (i % m != 0) {
+                num1 += i
+            } else {
+                num2 += i
+            }
+        }
+
+        return num1 - num2
+    }
+}
