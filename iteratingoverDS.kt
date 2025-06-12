a66f01e5b666d6088881e18344916c91508ead34
Dhananjay Ingole <145216339+dhananjayingole@users.noreply.github.com>
2025-06-12 18:32:21 +0530

    Create iteratingoverDS.kt

---
+++ iteratingoverDS.kt
@@ -0,0 +1,13 @@
+//iteration over Data Structure.
+fun main(args: Array<String>){
+  var language = arrayOf("Ruby", "kotlin", "python","Java")
+
+  for(item in language)
+  println(item)
+
+  // diifrent constructor in kotlin
+// 1. primary Constructor && secondary constructor.
+
+
+  
+  
