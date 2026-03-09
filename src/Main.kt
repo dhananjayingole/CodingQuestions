b1f65e3cab4da7f7c03c992b7363a9c15e63154d
Dhananjay Ingole <2023bcs116@sggs.ac.in>
2026-03-09 12:48:08 +0530

    Fixed code and preparing to push

---
+++ src/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+    println("Arguments Received: ${args.joinToString()}")
+    println("My name is Dhananjay Ingole")
+    println("I Am Learning Kotlin")
+
+    println(5)
+    println(258963)
+    println(25*5)
+}
