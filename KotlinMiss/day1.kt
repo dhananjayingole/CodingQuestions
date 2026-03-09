45e386a3b62c8a86873b7a914c0b33cfc75791fe
Dhananjay Ingole <2023bcs116@sggs.ac.in>
2026-03-09 12:46:47 +0530

    Initial commit from local folder

---
+++ KotlinMiss/day1.kt
@@ -0,0 +1,24 @@
+fun main(){
+    println("My name is DJUU")
+    println("I am 23 year Old")
+    Maths()
+    MonthAge()
+    RectangleArea()
+}
+
+
+fun Maths(){
+    println(25*3)
+}
+
+fun MonthAge(){
+    println(" My Age in months Old:" + 22 * 12)
+}
+
+fun RectangleArea(){
+    var length = 20
+    var ht = 10
+
+    var Area = length * ht
+    println(Area)
+}
