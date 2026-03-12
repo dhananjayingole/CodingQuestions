2e3757d5e4b49b472d72f897cb1622a107670777
Dhananjay Ingole <2023bcs116@sggs.ac.in>
2026-03-12 21:45:01 +0530

    Initial commit from local folder

--- src/KotlinLearn/Day4.kt
+++ src/KotlinLearn/Day4.kt
@@ -1,11 +1,17 @@
 package KotlinLearn
 
+import kotlin.math.max
+
 fun main(){
 //    createArray()
 //    ArrayWithConstructor()
-    NullArray()
+//    NullArray()
+//    iteratethroughArray()
+//    iteratethroughArray()
+    twoDimMatrix()
+    val result = linearSearch()
+    println("Target found: $result")
 }
-
 
 fun createArray(){
     val numbers = arrayOf(1,2,3,4,5)
@@ -44,4 +50,67 @@ fun NullArray(){
 
     val nullArray = arrayOfNulls<String>(3)
     println(nullArray.joinToString())
+}
+
+fun iteratethroughArray(){
+    val fruits = arrayOf("Apple", "WaterMelon", "Arjun", "DJU")
+
+//  without idx.
+    for(fruit in fruits){
+        println(fruit)
+    }
+
+//  with index
+    for ((index, fruit) in fruits.withIndex()) {
+        println("$index: $fruit")
+    }
+
+//    using forEach
+    fruits.forEach { fruit ->
+        println(fruit)
+    }
+
+//    using forEachIndexed loop
+    fruits.forEachIndexed { index, fruit ->
+        println("[$index] = $fruit")
+    }
+}
+
+fun linearSearch(): Boolean {
+    val numbers = intArrayOf(10, 20, 30, 40, 50)
+    val target = 30
+
+    for (integer in numbers) {
+        if (integer == target) {
+            return true  // Found the target
+        }
+    }
+    return false  // Target not found after checking all elements
+}
+
+fun twoDimMatrix(): Int {
+    val matrix = arrayOf(
+        intArrayOf(1,2,3),
+        intArrayOf(4,5,6),
+        intArrayOf(7,8,9)
+    )
+
+    var sum = 0
+    val rows = matrix.size
+    val cols = matrix[0].size
+
+//    Sort each row in Descending order.
+    for(i in 0 until rows){
+        matrix[i].sortDescending()
+    }
+
+//    traverse col wise
+    for(j in 0 until cols){
+        var maxCol = 0
+        for(i in 0 until rows){
+            maxCol = maxOf(maxCol, matrix[i][j])
+        }
+        sum+= maxCol
+    }
+    return sum
 }
